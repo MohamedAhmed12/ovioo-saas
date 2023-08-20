b482b99310a2f5636e689618eb2a93e98f12a1b7
MohamedAhmed12 <mohamed7el17@gmail.com>
2023-08-20 18:18:37 +0300

    feat(fe-faq): create FAQ section UI

--- client/src/components/BottomWrapper.tsx
+++ client/src/components/BottomWrapper.tsx
@@ -1,9 +1,12 @@
+"use client";
 import "@/styles/components/bottom-wrapper.scss";
 import Footer from "./Footer";
+import FAQ from "./FAQ";
 
 export default function BottomWrapper() {
     return (
         <div className="bottom-wrapper w-full">
+            <FAQ />
             <Footer />
         </div>
     );
