76890eb3279bcf97ec663c2ad58fa09d2b994024
MohamedAhmed12 <mohamed7el17@gmail.com>
2023-08-21 18:33:44 +0300

    feat(fe-getstarted): get started section UI

--- client/src/components/BottomWrapper.tsx
+++ client/src/components/BottomWrapper.tsx
@@ -2,11 +2,13 @@
 import "@/styles/components/bottom-wrapper.scss";
 import Footer from "./Footer";
 import FAQ from "./FAQ";
+import GetStarted from "./GetStarted";
 
 export default function BottomWrapper() {
     return (
         <div className="bottom-wrapper w-full">
             <FAQ />
+            <GetStarted/>
             <Footer />
         </div>
     );
---
+++ client/src/components/GetStarted.tsx
@@ -0,0 +1,50 @@
+import "@/styles/components/get-started.scss";
+
+export default function GetStarted() {
+    return (
+        <div className="section get-started">
+            <div className="cta_banner-wrap">
+                <div className="dog_wrapper">
+                    <img
+                        src="https://uploads-ssl.webflow.com/602d59da29b66668b8758391/60e37a1e3bf7f815da084a1c_new-moris-awesomic-dog.png"
+                        loading="lazy"
+                        width="295"
+                        height="228"
+                        alt="ovioo dog"
+                        className="ovioo-dog"
+                    />
+                </div>
+            </div>
+            <div className="cta_outline">
+                <div className="gradient-c-stroke">
+                    <div className="cta_container">
+                        <div className="cta__heading-wrapper">
+                            <h2 className="cta_heading">
+                                Add a <strong className="font-medium">Pro Designer</strong> to
+                                Your Team <strong className="font-medium">in Minutes</strong>,
+                                <strong className="font-medium"> Not Weeks</strong>.
+                            </h2>
+                        </div>
+                        <div className="cta_btn-container">
+                            <a
+                                href="/register"
+                                className="ghost-button-wrap new-cta_7for7 hidden w-inline-block"
+                            >
+                                <div className="ghost-button new-cta">
+                                    <div className="buttons-l">Get started</div>
+                                </div>
+                            </a>
+                            <a href="/demo" className="button-wrapper book-demo w-inline-block">
+                                <div className="button-outline">
+                                    <div className="cta book-demo">
+                                        <p className="buttons-l dark">Book a Demo</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
