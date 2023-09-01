0ae29858b8163840faef0deff31c307362cc00c1
MohamedAhmed12 <mohamed7el17@gmail.com>
2023-09-02 01:52:45 +0300

    fix(fe-how-it-works): polish how it works page style

--- client/src/app/(unauth)/how-it-works/page.tsx
+++ client/src/app/(unauth)/how-it-works/page.tsx
@@ -1,152 +1,106 @@
 "use client";
 
+import HowItWorksStepper from "@/components/HowItWorks/HowItWorksStepper";
 import "@/styles/app/unauth/how-it-works.scss";
-
-import { Divider } from '@mui/material';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
+import { Typography } from "@mui/material";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
 import Image from "next/image";
 
 export default function HowItWorks() {
     const srcset = (image: string, size: number, rows: number = 1, cols: number = 1) => {
         return {
             src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
-            srcSet: `${image}?w=${size * cols}&h=${size * rows
-                }&fit=crop&auto=format&dpr=2 2x `,
+            srcSet: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format&dpr=2 2x `,
         };
-    }
+    };
 
     const itemData = [
         {
-            img: '/images/smile.jpg',
-            title: 'Breakfast',
+            img: "/images/smile.jpg",
+            title: "Breakfast",
+            rows: 1,
+            cols: 1,
+        },
+        {
+            img: "/images/smile.jpg",
+            title: "Burger",
+            rows: 1,
+            cols: 1,
+        },
+        {
+            img: "/images/smile.jpg",
+            title: "Camera",
             rows: 2,
             cols: 2,
         },
         {
-            img: '/images/smile.jpg',
-            title: 'Burger',
-        },
-        {
-            img: '/images/smile.jpg',
-            title: 'Camera',
-        },
-        {
-            img: '/images/smile.jpg',
-            title: 'Coffee',
+            img: "/images/smile.jpg",
+            title: "Coffee",
             cols: 2,
         },
         {
-            img: '/images/smile.jpg',
-            title: 'Hats',
-            cols: 2,
-        },
-        {
-            img: '/images/smile.jpg',
-            title: 'Honey',
-            author: '@arwinneil',
+            img: "/images/smile.jpg",
+            title: "Breakfast",
             rows: 2,
             cols: 2,
         },
         {
-            img: '/images/smile.jpg',
-            title: 'Basketball',
+            img: "/images/smile.jpg",
+            title: "Honey",
+            author: "@arwinneil",
+            rows: 1,
+            cols: 2,
         },
         {
-            img: '/images/smile.jpg',
-            title: 'Fern',
+            img: "/images/smile.jpg",
+            title: "Breakfast",
+            rows: 1,
+            cols: 1,
+        },
+        {
+            img: "/images/smile.jpg",
+            title: "Burger",
+            rows: 1,
+            cols: 1,
         },
     ];
 
     return (
-        <><div className="hero-how-to">
-            <div className="hero-container">
-                <div className="h1-wrapper m-show__xs-hidden">
-                    <div className="h1 test-hero">
-                        <strong>GET DAILY,</strong>
-                        <strong className="text-gradient__pink-yellow">HIGH-END</strong>
-                        <strong className="text-gradient__green-blue">DESIGN</strong>
-                        <strong>DONE WITH AWESOMIC</strong>
-                    </div>
-                </div>
-                <div className="h1-wrapper m-hidden">
-                    <h1 className="h1 hero">
-                        <strong>GET DAILY,</strong>
-                        <strong className="text-gradient-h1">HIGH-END DESIGN</strong>
-                        <strong>DONE WITH AWESOMIC</strong>
-                    </h1>
-                </div>
-                <div className="h1-wrapper xs-show">
-                    <div className="h1 test-hero">
-                        <strong>GET DAILY,</strong>
-                        <strong className="gradient_pink">HIGH-</strong>
+        <div className="how-it-works">
+            <div className="intro flex flex-col items-center pr-[10px] pl-[10px] relative">
+                <div className="container title uppercase text-center">
+                    <Typography variant="h2">
+                        <strong>get daily, </strong>
+                        <strong className="text-gradient">high-end design</strong>
                         <br />
-                        <strong className="text-gradient__yellow">END</strong>
-                        <strong className="text-gradient__green-blue">DESIGN</strong>
-                        <strong>DONE WITH AWESOMIC</strong>
-                    </div>
+                        <strong>done with ovioo</strong>
+                    </Typography>
                 </div>
+
+                <HowItWorksStepper />
             </div>
-            <div className="hero-facts-wrapper">
-                <div className="step-wrapper">
-                    <div>
-                        <Image src="/assets/images/sign-up.png" loading="lazy" height="75" width="75" alt="add icon" className="steps-ico" />
-                    </div>
-                    <div className="steps-text">
-                        <div className="body-text-l">Sign up and</div>
-                        <div className="h6">start creating tasks</div>
-                    </div>
-                </div>
-                <div className="steps-divider">
-                </div>
-                <div className="step-wrapper">
-                    <div>
-                        <Divider
-                            variant="fullWidth"
-                            component="li"
-                            light={false}
-                            className="list-none list-divider mb-6 mt-6"
-                        />
-                        <Image src="/assets/images/get-matched.png" loading="lazy" width="75" height="75" alt="user icon" className="steps-ico" />
-                    </div>
-                    <div className="steps-text">
-                        <div className="h6">Get matched</div>
-                        <div className="body-text-l">with a pro designer</div>
-                    </div>
-                </div>
-                <div className="steps-divider">
-                </div>
-                <div className="step-wrapper">
-                    <div>
-                        <Divider
-                            variant="fullWidth"
-                            component="li"
-                            light={false}
-                            className="list-none list-divider mb-6 mt-6"
-                        />
-                        <Image src="/assets/images/design-updates.png" loading="lazy" width="75" height="75" alt="user icon" className="steps-ico" />
-                    </div>
-                    <div className="steps-text">
-                        <div className="body-text-l">Get design updates</div>
-                        <div className="h6">every 24 business hours</div>
-                    </div>
-                </div>
+            <div className="container w-full p-5 flex justify-center">
+                <iframe
+                    width="1200"
+                    height="480"
+                    src="https://www.youtube.com/embed/IEuHpriOVzE"
+                    title="Positive Vibes Music 🌻 Top 100 Chill Out Songs Playlist | Romantic English Songs With Lyrics"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowFullScreen
+                ></iframe>
             </div>
-        </div>
-            <div className="section">
-                <iframe width="853" height="480" src="https://www.youtube.com/embed/IEuHpriOVzE" title="Positive Vibes Music 🌻 Top 100 Chill Out Songs Playlist | Romantic English Songs With Lyrics" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-            </div>
-            <div className="new-title_wrapper center">
-                <h2 className="h-40 m-34 s-28">What You Get With Awesomic</h2>
-            </div>
-            <div className="section">
+
+            <div className="what-you-get text-[40px] font-medium text-center mt-36 mb-20 flex  flex-col items-center">
+                <h2 className="mb-20">What You Get With Ovioo</h2>
                 <ImageList
-                    sx={{ width: 1000, height: 1200, gridAutoColumns: 1, borderRadius: 8 }}
+                    sx={{ gridAutoColumns: 1 }}
                     variant="quilted"
                     cols={4}
-                    grid-column-gap='60px'
-                    grid-row-gap='60px'
-                    gap={30}
+                    grid-column-gap="60px"
+                    grid-row-gap="60px"
+                    gap={60}
+                    className="w-full pr-5 pl-5"
                 >
                     {itemData.map((item) => (
                         <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
@@ -162,6 +116,6 @@ export default function HowItWorks() {
                     ))}
                 </ImageList>
             </div>
-        </>
-    )
+        </div>
+    );
 }
--- client/src/components/MediaWrapper.tsx
+++ client/src/components/MediaWrapper.tsx
@@ -4,20 +4,43 @@ import CardCover from "@mui/joy/CardCover";
 import Typography from "@mui/joy/Typography";
 import { ReactNode } from "react";
 
-export default function MediaWrapper({ media, title }: { media: ReactNode; title?: string }) {
+export default function MediaWrapper({
+    media,
+    title,
+    width,
+    height,
+}: {
+    media: ReactNode;
+    title?: string;
+    width: string;
+    height: string;
+}) {
     return (
-        <Card className="w-full h-full rounded-xl" style={{ minWidth: "300" }}>
-            <CardCover>{media}</CardCover>
-            <CardContent>
-                <Typography
-                    level="body-lg"
-                    fontWeight="lg"
-                    textColor="#fff"
-                    mt={{ xs: 12, sm: 18 }}
-                >
-                    {title}
-                </Typography>
-            </CardContent>
-        </Card>
+        <div className="container">
+            <Card
+                className="media-wrapper rounded-xl m-auto"
+                style={{
+                    minWidth: "300px",
+                    width: `${width}px`,
+                    height: `${height}px`,
+                    backgroundColor: "transparent",
+                }}
+            >
+                <CardCover>{media}</CardCover>
+
+                {title && (
+                    <CardContent>
+                        <Typography
+                            level="body-lg"
+                            fontWeight="lg"
+                            textColor="#fff"
+                            mt={{ xs: 12, sm: 18 }}
+                        >
+                            {title}
+                        </Typography>
+                    </CardContent>
+                )}
+            </Card>
+        </div>
     );
 }
