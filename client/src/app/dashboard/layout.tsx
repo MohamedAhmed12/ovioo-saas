d93fbf1649b8ec70cdeb40c00a1e7ff30e281f66
MohamedAhmed12 <mohamed7el17@gmail.com>
2023-09-05 21:53:01 +0300

    feat(fe-mode): create dark mode button for dashboard

--- client/src/app/dashboard/layout.tsx
+++ client/src/app/dashboard/layout.tsx
@@ -1,22 +1,10 @@
-import "@/styles/app/unauth/layout.scss";
-
-import Footer from "@/components/Footer";
-import NavBar from "@/components/NavBar";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-    title: " Ovioo",
-};
+"use client";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between main-layout">
-            <NavBar/>
+        <main className="flex min-h-screen flex-col items-center justify-between dashboard-main-layout">
             {children}
-            <Footer />
         </main>
     );
 }
--- client/src/app/dashboard/task/page.tsx
+++ client/src/app/dashboard/task/page.tsx
@@ -1,7 +1,22 @@
+"use client";
+
+import { useAppDispatch, useAppSelector } from "@/hooks/redux";
+import { ModeEnum } from "@/interfaces/store/main";
+import { toggleMode } from "@/store/features/main";
 import "@/styles/app/unauth/home.scss";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import { IconButton } from "@mui/joy";
 
 export default function Task() {
+    const mode = useAppSelector((state) => state.mainReducer.mode);
+    const dispatch = useAppDispatch();
+
     return (
-     <div>s</div>
+        <div>
+            <IconButton sx={{ ml: 1 }} onClick={() => dispatch(toggleMode())}>
+                {mode === ModeEnum.Dark ? <Brightness7Icon /> : <Brightness4Icon />}
+            </IconButton>
+        </div>
     );
 }
