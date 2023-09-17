ec9437d19642e406164174930f7be546b34b0f49
MohamedAhmed12 <mohamed7el17@gmail.com>
2023-09-17 19:31:55 +0300

    fix(fe-project):  project modal card implementation

--- client/src/components/Dashboard/Project/AddNewProjectCardModal.tsx
+++ client/src/components/Dashboard/Project/AddNewProjectCardModal.tsx
@@ -14,6 +14,8 @@ export default function AddNewProjectCardModal({
     open: boolean;
     handleToggleModal: (e: MouseEvent<HTMLElement> | null) => void;
 }) {
+    const createProject = () => {};
+
     return (
         <div>
             <Modal
@@ -26,58 +28,61 @@ export default function AddNewProjectCardModal({
                 <DashBoardCard
                     headerTitle="add new project"
                     style={{ maxWidth: 500, padding: "45px 40px" }}
+                    handleSubmit={createProject}
                 >
-                    <div className="flex flex-col lg:flex-row justify-between items-center w-full mb-8">
-                        <div className="basis-3/12">
-                            <Image
-                                src="https://picsum.photos/id/12/400/400"
-                                width="500"
-                                height="500"
-                                alt="profile"
-                                className="rounded-full max-w-full"
+                    <>
+                        <div className="flex flex-col lg:flex-row justify-between items-center w-full mb-8">
+                            <div className="basis-3/12">
+                                <Image
+                                    src="https://picsum.photos/id/12/400/400"
+                                    width="500"
+                                    height="500"
+                                    alt="profile"
+                                    className="rounded-full max-w-full"
+                                />
+                            </div>
+
+                            <Button
+                                type="submit"
+                                className="basis-6/12 new-project-btn capitalize text-white border-solid border-2 border-[--dashboard-primary] py-2 text-base tracking-widest"
+                            >
+                                + project cover
+                            </Button>
+                        </div>
+                        <div className="mb-8">
+                            <TextField
+                                className="dashboard-input"
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="title"
+                                label="Title"
+                                name="title"
+                                autoComplete="title"
+                                autoFocus
+                            />
+                            <TextField
+                                className="dashboard-input"
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="additional-info"
+                                label="Additional Information"
+                                type="email"
+                                id="email"
+                                autoComplete="current-password"
                             />
                         </div>
-
-                        <Button
-                            type="submit"
-                            className="basis-6/12 new-project-btn capitalize text-white border-solid border-2 border-[--dashboard-primary] py-2 text-base tracking-widest"
-                        >
-                            + project cover
-                        </Button>
-                    </div>
-                    <div className="mb-8">
-                        <TextField
-                            className="dashboard-input"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="title"
-                            label="Title"
-                            name="title"
-                            autoComplete="title"
-                            autoFocus
-                        />
-                        <TextField
-                            className="dashboard-input"
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="additional-info"
-                            label="Additional Information"
-                            type="email"
-                            id="email"
-                            autoComplete="current-password"
-                        />
-                    </div>
-                    <div className="flex w-full justify-end mt-6">
-                        <Button
-                            type="submit"
-                            className="dashboard__btn text-white capitalize px-9 py-3 font-bold text-base tracking-wider"
-                        >
-                            create project
-                            <EastIcon className="dark:text-white ml-2" />
-                        </Button>
-                    </div>
+                        <div className="flex w-full justify-end mt-6">
+                            <Button
+                                type="submit"
+                                className="dashboard__btn text-white capitalize px-9 py-3 font-bold text-base tracking-wider"
+                            >
+                                create project
+                                <EastIcon className="dark:text-white ml-2" />
+                            </Button>
+                        </div>
+                    </>
                 </DashBoardCard>
             </Modal>
         </div>
