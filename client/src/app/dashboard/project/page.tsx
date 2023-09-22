82fbe7861564af78a02ac37bf93093141250534c
MohamedAhmed12 <mohamed7el17@gmail.com>
2023-09-22 05:23:24 +0300

    fix the build

--- client/src/app/dashboard/project/page.tsx
+++ client/src/app/dashboard/project/page.tsx
@@ -3,24 +3,27 @@
 import AddNewProjectCard from "@/components/Dashboard/Project/AddNewProjectCard";
 import AddNewProjectCardModal from "@/components/Dashboard/Project/AddNewProjectCardModal";
 import ProjectCard from "@/components/Dashboard/Project/ProjectCard";
+import { TaskStatus } from "@/interfaces";
 import { useState } from "react";
 
 export default function Projects() {
     const [open, setOpen] = useState(false);
-    const projects= [
+    const projects = [
         {
             id: 1,
             name: "first project",
             tasks: [
                 {
+                    id: 1,
                     title: "First task",
                     description: "",
-                    status: "Todo",
+                    status: TaskStatus.InQueue,
                 },
                 {
+                    id: 2,
                     title: "Second task",
                     description: "",
-                    status: "Todo",
+                    status: TaskStatus.InQueue,
                 },
             ],
         },
@@ -29,14 +32,16 @@ export default function Projects() {
             name: "second project",
             tasks: [
                 {
+                    id: 1,
                     title: "First task",
                     description: "",
-                    status: "Todo",
+                    status: TaskStatus.InQueue,
                 },
                 {
+                    id: 2,
                     title: "Second task",
                     description: "",
-                    status: "Todo",
+                    status: TaskStatus.InQueue,
                 },
             ],
         },
@@ -49,7 +54,11 @@ export default function Projects() {
     return (
         <div className="flex justify-start flex-wrap">
             {projects.map((project, i) => (
-                <ProjectCard project={project} key={i} actionURL={`/dashboard/project/${project.id}`} />
+                <ProjectCard
+                    project={project}
+                    key={i}
+                    actionURL={`/dashboard/project/${project.id}`}
+                />
             ))}
 
             <AddNewProjectCard handleToggleModal={handleToggleModal} />
