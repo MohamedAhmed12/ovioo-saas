5db4977c901d7f0d9a1fce2e23d0820b7e19f73e
MohamedAhmed12 <mohamed7el17@gmail.com>
2023-09-25 09:14:58 +0300

    fix(fe-apollo): make sure my app has the correct uri for the server graphql

--- client/src/app/api/auth/[...nextauth]/apollo-client.ts
+++ client/src/app/api/auth/[...nextauth]/apollo-client.ts
@@ -2,13 +2,15 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 
 let client: ApolloClient<any> | null = null;
 export const getClient = () => {
+    const link = new HttpLink({
+        uri: `http://localhost:3000/graphql`,
+    });
+
     // Create new client if there is no existing one
     // or if we are running on server
-    if (!client || typeof window === "undefined") {
+    if (!client || typeof window === "undefined" || client.link?.options?.uri != link) {
         client = new ApolloClient({
-            link: new HttpLink({
-                uri: `${process.env.BASE_URL}/graphql`,
-            }),
+            link,
             cache: new InMemoryCache(),
         });
     }
