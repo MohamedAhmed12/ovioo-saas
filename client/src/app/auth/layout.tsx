262fc089b211e8d60b581a71652fcdabaa0a1783
MohamedAhmed12 <mohamed7el17@gmail.com>
2023-09-27 10:41:51 +0300

    feat(fe-auth): toast msg if sso provider return with error on auth layout

--- client/src/app/auth/layout.tsx
+++ client/src/app/auth/layout.tsx
@@ -1,8 +1,22 @@
+"use client";
+
 import LeftSideCover from "@/components/Auth/LeftSideCover";
 import "@/styles/app/auth/layout.scss";
-import { Toaster } from "react-hot-toast";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
+    const searchParams = useSearchParams();
+    const callbackUrl = searchParams.get("error");
+
+    useEffect(() => {
+        if (callbackUrl) {
+            toast.error("Something went wrong!");
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-between auth-layout">
             <div className="login flex w-full">
