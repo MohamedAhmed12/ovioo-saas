e193fb8c36dea5c47a3fe6f729b209173bd98858
MohamedAhmed12 <mohamed7el17@gmail.com>
2023-09-28 06:24:13 +0300

    feat(fe-company): fetch user's company

--- client/src/app/dashboard/company/page.tsx
+++ client/src/app/dashboard/company/page.tsx
@@ -1,104 +1,153 @@
-'use client';
-import DashBoardCard from '@/components/DashBoardCard';
-import { styled, TextField } from '@mui/material';
-import Button from '@mui/material/Button';
-import { FormEvent, useState } from 'react';
-import { TextareaAutosize } from '@mui/base/TextareaAutosize';
+"use client";
+import { getClient } from "@/app/api/apollo-client";
+import DashBoardCard from "@/components/DashBoardCard";
+import { useInput } from "@/hooks/useInput";
+import { gql, useQuery } from "@apollo/client";
+import { TextField } from "@mui/material";
+import Button from "@mui/material/Button";
+import { FormEvent, useEffect } from "react";
+
+const FetchProfile = gql`
+    query ($id: Float!) {
+        findProfile(id: $id) {
+            id
+            company_name
+            company_links
+            company_website
+            target_audience
+            business_info
+            push_notification_enabled
+            mail_notification_enabled
+            created_at
+            updated_at
+        }
+    }
+`;
 
 export default function Company() {
-  const label = { inputProps: { 'aria-label': 'Switch demo' } };
+    const client = getClient();
+    const { loading, error, data } = useQuery(FetchProfile, { client, variables: { id: 1 } });
+    const {
+        value: company_name,
+        setValue: setValueCompanyName,
+        bind: bindCompanyName,
+    } = useInput("");
+    const {
+        value: company_website,
+        setValue: setValueCompanyWebsite,
+        bind: bindCompanyWebsite,
+    } = useInput("");
+    const {
+        value: business_info,
+        setValue: setValueBusinessInfo,
+        bind: bindBusinessInfo,
+    } = useInput("");
+    const {
+        value: target_audience,
+        setValue: setValueTargetAudience,
+        bind: bindTargetAudience,
+    } = useInput("");
+    const {
+        value: company_links,
+        setValue: setValueCompanyLinks,
+        bind: bindCompanyLinks,
+    } = useInput("");
 
-  const ValidationTextField = styled(TextField)({
-    '& input:valid + fieldset': {
-      borderColor: '#E0E3E7',
-      borderWidth: 1,
-    },
-    '& input:invalid + fieldset': {
-      borderColor: 'red',
-      borderWidth: 1,
-    },
-    '& input:valid:focus + fieldset': {
-      borderLeftWidth: 4,
-      padding: '4px !important', // override inline-style
-    },
-  });
+    useEffect(() => {
+        console.log("loain");
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
-  return (
-    <div className="company-card px-40 flex flex-col lg:flex-col w-full">
-      <DashBoardCard handleSubmit={handleSubmit} headerTitle="profile settings">
-        <>
-          <div className="flex flex-row">
-            <div className="w-full flex flex-col">
-              <TextField
-                className="dashboard-input"
-                margin="normal"
-                fullWidth
-                id="Company-name"
-                label="Company name"
-                name="Company name"
-                autoComplete="Company-name"
-                autoFocus
-              />
-              <TextField
-                className="dashboard-input"
-                margin="normal"
-                fullWidth
-                name=" link"
-                label="Website link"
-                type="link"
-                id="link"
-                autoComplete="Website-link"
-              />
-                 <TextField
-                className="dashboard-input"
-                margin="normal"
-                fullWidth
-                name="what is your business About"
-                label="what is your business About?"
-                type="what is your business About"
-                id="what-is-your-business-About"
-                multiline
-              />
-              <TextField
-                className="dashboard-input"
-                margin="normal"
-                fullWidth
-                name="client"
-                label="describe your client/ target audience"
-                type="client"
-                id="client"
-                multiline
-              />
-              <TextField
-                className="dashboard-input"
-                margin="normal"
-                fullWidth
-                name="Other links"
-                label="Other links"
-                type="Other links"
-                id="Other-links"
-                multiline
-              />
+        if (!loading) {
+            setValueCompanyName(data.findProfile.company_name || "");
+            setValueCompanyWebsite(data.findProfile.company_website || "");
+            setValueBusinessInfo(data.findProfile.business_info || "");
+            setValueTargetAudience(data.findProfile.target_audience || "");
+            setValueCompanyLinks(data.findProfile.company_links || "");
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [loading, data]);
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get("email"),
+            password: data.get("password"),
+        });
+    };
+
+    if (error) throw new Error(JSON.stringify(error));
+
+    return (
+        !loading &&
+        data && (
+            <div className="company-card px-40 flex flex-col lg:flex-col w-full">
+                <DashBoardCard handleSubmit={handleSubmit} headerTitle="profile settings">
+                    <>
+                        <div className="flex flex-row">
+                            <div className="w-full flex flex-col">
+                                <TextField
+                                    className="dashboard-input"
+                                    margin="normal"
+                                    fullWidth
+                                    id="Company-name"
+                                    label="Company name"
+                                    name="Company name"
+                                    {...bindCompanyName}
+                                    autoFocus
+                                />
+                                <TextField
+                                    className="dashboard-input"
+                                    margin="normal"
+                                    fullWidth
+                                    name=" link"
+                                    label="Website link"
+                                    type="link"
+                                    id="link"
+                                    {...bindCompanyWebsite}
+                                />
+                                <TextField
+                                    className="dashboard-input"
+                                    margin="normal"
+                                    fullWidth
+                                    name="what is your business About"
+                                    label="what is your business About?"
+                                    type="what is your business About"
+                                    id="what-is-your-business-About"
+                                    multiline
+                                    {...bindBusinessInfo}
+                                />
+                                <TextField
+                                    className="dashboard-input"
+                                    margin="normal"
+                                    fullWidth
+                                    name="client"
+                                    label="describe your client/ target audience"
+                                    type="client"
+                                    id="client"
+                                    multiline
+                                    {...bindTargetAudience}
+                                />
+                                <TextField
+                                    className="dashboard-input"
+                                    margin="normal"
+                                    fullWidth
+                                    name="Other links"
+                                    label="Other links"
+                                    type="Other links"
+                                    id="Other-links"
+                                    multiline
+                                    {...bindCompanyLinks}
+                                />
+                            </div>
+                        </div>
+                        <div className="flex w-full justify-end mt-6">
+                            <Button type="submit" className="bg-[--dashboard-primary] text-white ">
+                                update
+                            </Button>
+                        </div>
+                    </>
+                </DashBoardCard>
             </div>
-          </div>
-          <div className="flex w-full justify-end mt-6">
-            <Button
-              type="submit"
-              className="bg-[--dashboard-primary] text-white "
-            >
-              update
-            </Button>
-          </div>
-        </>
-      </DashBoardCard>
-    </div>
-  );
+        )
+    );
 }
--- client/src/app/default.tsx
+++
@@ -1,3 +0,0 @@
-export default function Default() {
-    return <p className="text-6xl text-red-950">Default...</p>;
-}
