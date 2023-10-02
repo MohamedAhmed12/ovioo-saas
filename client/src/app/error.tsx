e27ba20322248ebf0ad227e7fb10c8dc30888e93
MohamedAhmed12 <mohamed7el17@gmail.com>
2023-10-02 08:22:30 +0300

    fix(fe-error): add custom error in error page

--- client/src/app/error.tsx
+++ client/src/app/error.tsx
@@ -1,6 +1,8 @@
 "use client"; // Error components must be Client Components
 
-import { useEffect } from "react";
+import { useAppSelector } from "@/hooks/redux";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Error({
     error,
@@ -9,14 +11,23 @@ export default function Error({
     error: Error & { digest?: string };
     reset: () => void;
 }) {
+    const router = useRouter();
+    const user = useAppSelector((state) => state.userReducer.user);
+    const [customError, setCustomError] = useState("");
+
     useEffect(() => {
-        // Log the error to an error reporting service
-        console.error(error);
-    }, [error]);
+        if (!user) {
+            setCustomError("You have to login in order to view this page.");
+            const timer = setTimeout(() => router.push("/auth/login"), 1000);
+            return () => clearTimeout(timer);
+        }
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [error, user]);
 
     return (
         <div className="bg-black">
-            <h2>Something went wrong!</h2>
+            <h2>{customError || "Something went wrong!"}</h2>
             <button
                 onClick={
                     // Attempt to recover by trying to re-render the segment
