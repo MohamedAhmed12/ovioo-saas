b2826a744852199936c1d50fa0605c0226f29a66
MohamedAhmed12 <mohamed7el17@gmail.com>
2023-10-04 06:46:11 +0300

    feat(be-auth): Custom validation class to validate password confirmation

--- server/src/user/dto/register.dto.ts
+++ server/src/user/dto/register.dto.ts
@@ -6,7 +6,9 @@ import {
   IsStrongPassword,
   MaxLength,
   MinLength,
+  Validate,
 } from 'class-validator';
+import { MatchesProperty } from 'src/shared/validators/matches-property.validator';
 import { LoginDto } from './login.dto';
 
 @InputType()
@@ -26,6 +28,9 @@ export class RegisterDto extends LoginDto {
   @MaxLength(23)
   @MinLength(8)
   @Field()
+  @Validate(MatchesProperty, ['password'], {
+    message: 'Password confirmation must match password',
+  })
   password_confirmation: string;
 
   @IsString()
