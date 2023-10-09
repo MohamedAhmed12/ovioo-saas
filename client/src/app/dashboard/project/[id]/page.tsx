8934193bccfc94f7184b8a49810dd2f0049f26af
MohamedAhmed12 <mohamed7el17@gmail.com>
2023-10-09 05:19:58 +0300

    feat(fe-project): show single project frontend implementation

--- client/src/app/dashboard/project/[id]/page.tsx
+++ client/src/app/dashboard/project/[id]/page.tsx
@@ -1,10 +1,10 @@
 import ProjectDetailedCard from "@/components/Dashboard/Project/ProjectDetailedCard";
 import ProjectTasksTable from "@/components/Dashboard/Project/ProjectTasksTable";
 
-export default function ViewProject() {
+export default function ViewProject({ params: { id } }: { params: { id: string } }) {
     return (
         <div className="view-project">
-            <ProjectDetailedCard />
+            <ProjectDetailedCard id={id} />
             <ProjectTasksTable />
         </div>
     );
