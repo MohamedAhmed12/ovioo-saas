91fc2a498657e853097f88d492dc541f50a2f4e9
MohamedAhmed12 <mohamed7el17@gmail.com>
2024-01-10 04:55:44 +0200

    fix(task): task created subscription

--- src/task/task.resolver.ts
+++ src/task/task.resolver.ts
@@ -1,5 +1,12 @@
 import { UseGuards } from '@nestjs/common';
-import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Context,
+  Mutation,
+  Query,
+  Resolver,
+  Subscription,
+} from '@nestjs/graphql';
 import { AuthGuard } from 'src/shared/middlewares/auth.guard';
 import { User } from 'src/user/user.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -7,10 +14,15 @@ import { UpdateTaskDto } from './dto/update-task.dto';
 import { TaskType } from './task-type.entity';
 import { Task } from './task.entity';
 import { TaskService } from './task.service';
+import { PubSub } from 'graphql-subscriptions';
 
 @Resolver(() => Task)
 export class TaskResolver {
-  constructor(private readonly taskService: TaskService) {}
+  private pubSub: PubSub;
+
+  constructor(private readonly taskService: TaskService) {
+    this.pubSub = new PubSub();
+  }
 
   @UseGuards(AuthGuard)
   @Query(() => [TaskType])
@@ -36,7 +48,12 @@ export class TaskResolver {
     @Context('user') authUser: User,
     @Args('data') data: CreateTaskDto,
   ) {
-    return await this.taskService.createTask(data, authUser);
+    const task = await this.taskService.createTask(data, authUser);
+    this.pubSub.publish('taskCreated', {
+      taskCreated: task,
+    });
+
+    return task;
   }
 
   @UseGuards(AuthGuard)
@@ -49,5 +66,16 @@ export class TaskResolver {
   @Mutation(() => Boolean)
   async deleteTask(@Args('id') id: string) {
     return await this.taskService.deleteTask(id);
+  }
+
+  @UseGuards(AuthGuard)
+  @Subscription(() => Task, {
+    filter: async (payload: any, variables: any, context: any) => {
+      const taskTeam = await payload.taskCreated.team;
+      return taskTeam.id == context.user.team.id;
+    },
+  })
+  taskCreated() {
+    return this.pubSub.asyncIterator('taskCreated');
   }
 }
