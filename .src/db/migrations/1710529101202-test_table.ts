d4bbbe23612e05750698f6988f28468858c7fcbe
MohamedAhmed12 <mohamed7el17@gmail.com>
2024-03-15 22:26:03 +0200

    feat(DB): migration scripts
     -  generate migration file script
     -  run migration script
     -  revert migration script
     -  create new  migration file script
     - data-source file for typeorm scripts

---
+++ .src/db/migrations/1710529101202-test_table.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class TestTable1710529101202 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
